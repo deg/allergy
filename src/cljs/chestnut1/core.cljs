bbbf13cd5d9aacb67311dbb7a9851ef9ea6c3037
David Goldfarb <deg@degel.com>
2014-12-10 22:44:30 +0200

    Check-in of mostly broken cruft, playing with multi-page. Here only to archive this path before I abandon it.

--- src/cljs/chestnut1/core.cljs
+++ src/cljs/chestnut1/core.cljs
@@ -36,63 +36,6 @@
    (input label type id)
    (doall (map #(apply errmsg-bar id %) (partition 2 err-handlers)))])
 
-(def form-template
-  [:div
-   (row
-    "comments"
-    [:textarea.form-control
-     {:field :textarea :id :comments}])
-
-   [:hr]
-   (input "kg" :numeric :weight-kg)
-   (input "lb" :numeric :weight-lb)
-
-   [:hr]
-   [:h3 "BMI Calculator"]
-   (input "height" :numeric :height)
-   (input "weight" :numeric :weight)
-   (row "BMI"
-        [:input.form-control
-         {:field :numeric :fmt "%.2f" :id :bmi :disabled true}])
-   [:hr]
-
-   (row "isn't data binding lovely?"
-        [:input {:field :checkbox :id :databinding.lovely}])
-   [:label
-    {:field :label
-     :preamble "The level of awesome: "
-     :placeholder "N/A"
-     :id :awesomeness}]
-
-   ;; [:input {:field :range :min 1 :max 10 :id :awesomeness}]
-
-   ;; [:h3 "option list"]
-   [:div.form-group
-    [:label "pick an option"]
-    [:select.form-control {:field :list :id :many.options}
-     [:option {:key :foo} "foo"]
-     [:option {:key :bar} "bar"]
-     [:option {:key :baz} "baz"]]]
-
-   (radio
-    "Option one is this and that—be sure to include why it's great"
-    :radioselection :foo :a)
-   (radio
-    "Option two can be something else and selecting it will deselect option one"
-    :radioselection :foo :b)
-
-   ;; [:h3 "multi-select buttons"]
-   [:div.btn-group {:field :multi-select :id :every.position}
-    [:button.btn.btn-default {:key :left} "Left"]
-    [:button.btn.btn-default {:key :middle} "Middle"]
-    [:button.btn.btn-default {:key :right} "Right"]]
-
-   ;; [:h3 "single-select buttons"]
-   [:div.btn-group {:field :single-select :id :unique.position}
-    [:button.btn.btn-default {:key :left} "Left"]
-    [:button.btn.btn-default {:key :middle} "Middle"]
-    [:button.btn.btn-default {:key :right} "Right"]]])
-
 
 (defn header-dom [doc]
   [:h1 "My web page"])
@@ -106,7 +49,18 @@
 
 (defn app-dom [doc]
   [:div
-   [:h1 "I am the app; I am the walrus"]])
+   [:h1 "I am the app; I am the walrus"][:hr]
+   [:hr]
+   (input "kg" :numeric :weight-kg)
+   (input "lb" :numeric :weight-lb)
+   [:hr]
+   [:h3 "BMI Calculator"]
+   (input "height" :numeric :height)
+   (input "weight" :numeric :weight)
+   (row "BMI"
+        [:input.form-control
+         {:field :numeric :fmt "%.2f" :id :bmi :disabled true}])
+   ])
 
 
 (defn user-dom [doc]
@@ -133,58 +87,48 @@
    [:h1 "Document State"]
    [edn->hiccup @doc]])
 
-(defn other-dom [doc]
-  [:div
-   [:h1 "I am the other"]])
-
-(defn get-page-dom [key]
-  (case key
-    :app app-dom
-    :user user-dom
-    :guts guts-dom))
-
-(def doc (r/atom {:person {:first-name "John"
-                           :age 35
-                           :email "foo@bar.baz"}
-                  :weight 100
-                  :height 200
-                  :bmi 0.5
-                  :comments "some interesting comments\non this subject"
-                  :radioselection :b
-                  :position [:left :right]
-                  :pick-one :bar
-                  :unique {:position :middle}
-                  :pick-a-few [:bar :baz]
-                  :many {:options :bar}
-                  :menu-page :guts}))
+(defn page-dom [doc menu-key]
+  [:div {:key (str "page-" (menu-key @doc))}
+   (case (menu-key @doc)
+     :app (app-dom doc)
+     :user (user-dom doc)
+     :guts (guts-dom doc))])
 
 (defn page []
-  (fn []
-    [:div
-     [forms/bind-fields
-      [:div
-       (header-dom doc)
-       (menu-dom doc)
-       form-template]
-      doc
-      (fn [[id] value {:keys [weight-lb weight-kg] :as document}]
-        (cond
-         (= id :weight-lb)
-         (assoc document :weight-kg (/ value 2.2046))
-         (= id :weight-kg)
-         (assoc document :weight-lb (* value 2.2046))
-         (= id :menu-page)
-         (assoc document :menu-page value)
-         :else nil))
-      (fn [[id] value {:keys [height weight] :as document}]
-        (when (and (some #{id} [:height :weight]) weight height)
-          (assoc document :bmi (/ weight (* height height)))))]
-
-     [:button.btn.btn-default
-      {:on-click
-       #(if (empty? (get-in @doc [:person :first-name]))
-          (swap! doc assoc-in [:errors :first-name]"first name is empty"))}
-      ((get-page-dom (:menu-page @doc)) doc)]]))
+  (let [doc (r/atom {:person {:first-name "John"
+                              :age 35
+                              :email "foo@bar.baz"}
+                     :weight 100
+                     :height 200
+                     :bmi 0.5
+                     :menu-page :guts})]
+    (fn []
+      (js/console.log "DIV: "
+                      (str [:div
+                            (header-dom doc)
+                            (menu-dom doc)
+                            [page-dom doc :menu-page]]))
+      [:div.container-fluid
+       [:div.row-fluid
+        [:div.span6
+         [forms/bind-fields
+          [:div
+           (header-dom doc)
+           (menu-dom doc)
+           [page-dom doc :menu-page]]
+          doc
+          (fn [[id] value document]
+            (cond
+             (= id :weight-lb)
+             (assoc document :weight-kg (/ value 2.2046))
+             (= id :weight-kg)
+             (assoc document :weight-lb (* value 2.2046))
+             (= id :menu-page)
+             (assoc document :menu-page value)
+             :else nil))
+          (fn [[id] value {:keys [height weight] :as document}]
+            (when (and (some #{id} [:height :weight]) weight height)
+              (assoc document :bmi (/ weight (* height height)))))]]]])))
 
 (defn main []
   (r/render-component [page] (.getElementById js/document "app")))
